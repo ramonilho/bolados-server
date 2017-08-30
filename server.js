95bd94a95bcf7eb28a30c7d285ec3cee119a3b77
Ramon Honorio <ramonaqh@gmail.com>
2017-08-30 20:21:23 -0300

    Editing profile

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-// Get IP
+var express = require('express'),
+    app = express(),
+    stores = require('./stores')
+    users = require('./users');
+  
+app.use(express.static('public'));
+
+app.use('/stores', stores);
+app.use('/users', users);
+
+var server = app.listen(3000);
+
+var net = require('net');
+
+
+
+
+
+
+// Helper: Get IP Address
 function getNetworkIP(callback) {
   var socket = net.createConnection(80, 'www.google.com');
   socket.on('connect', function() {
@@ -9,20 +28,6 @@ function getNetworkIP(callback) {
     callback(e, 'error');
   });
 }
-
-var express = require('express'),
-    app = express(),
-    stores = require('./stores')
-    users = require('./users');
-
-app.use(express.static('public'));
-
-app.use('/stores', stores);
-app.use('/users', users);
-
-var server = app.listen(3000);
-
-var net = require('net');
 
 getNetworkIP(function (error, ip) {
     console.log('Servidor Bolado iniciado em: %s:%s', ip, server.address().port);
--- users.js
+++ users.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-
+    console.log("========== GET ==========")
     console.log("Request headers:");
     for (var header in req.headers) {
-        console.log("header["+header+"] ::: "+req.headers[header]);
+        console.log("# header["+header+"] ::: "+req.headers[header]);
     }
-    console.log("*************************");
 
     var requestEmail = req.headers["email"]
     var requestPassword = req.headers["password"]
@@ -21,7 +20,7 @@ router.get('/', function(req, res) {
     if (userResults.length > 0) {
       var user = userResults[0]
       if (user.password == requestPassword) {
-          console.log("Authenticated!")
+          console.log("User Authenticated!")
           res.send(user)
       } else {
           res.status(401).send({
@@ -33,8 +32,50 @@ router.get('/', function(req, res) {
             "message": "User not found"
         });
     }
+});
+
+router.put('/', function(req, res) {
+    console.log("========== PUT ==========")
+
+    console.log("*******\n>current USERS: "+JSON.stringify(USERS, null, "  ")+"\n*******");
 
 
+    // Parsing request body
+    var body = [];
+    req.on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+
+        body = Buffer.concat(body).toString();
+        var requestObj = JSON.parse(body);
+        console.log(requestObj);
+
+        var foundUser = false
+        
+        for (var user in USERS) {
+            if (USERS.hasOwnProperty(user)) {
+                var element = USERS[user];
+                if (element.id == requestObj.id) {
+                    USERS[user] = requestObj
+                    foundUser = true
+                    break
+                }
+            }
+        }
+    
+        if(foundUser == true){
+            console.log("Found user!")
+            res.send(USERS[0])
+        } else {
+            console.log("User not found")
+            res.status(401).send({
+                "message": "User not found"
+            });
+        }
+    
+        console.log("*******\n>after change USERS: "+JSON.stringify(USERS, null, "  ")+"\n*******");
+    });
+    
 });
 
 const USERS = [
@@ -44,6 +85,7 @@ const USERS = [
         "password" : "1234",
         "name" : "Ramon Honório",
         "creationDate" : "2017-01-01",
+        "streetAddress" : "Avenida Paulista, 1201 - São Paulo, SP",
         "photo" : null,
         "storeId" : 0
     },
@@ -51,8 +93,9 @@ const USERS = [
         "id" : 2,
         "email" : "eoq@email.com",
         "password" : "1234",
-        "name" : "Ramon Honório",
+        "name" : "EoQ user",
         "creationDate" : "2017-01-01",
+        "streetAddress" : "Avenida Paulista, 1200 - São Paulo, SP",
         "photo" : null,
         "storeId" : 0
     },
@@ -60,8 +103,9 @@ const USERS = [
         "id" : 3,
         "email" : "",
         "password" : "",
-        "name" : "Ramon Honório",
+        "name" : "Empty user",
         "creationDate" : "2017-01-01",
+        "streetAddress" : "Avenida Paulista, 1200 - São Paulo, SP",
         "photo" : null,
         "storeId" : 0
     }
